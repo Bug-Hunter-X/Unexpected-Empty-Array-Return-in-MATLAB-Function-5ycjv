2e1affce96e1815acedb645ef6ef9cb17bbdeee0
BugHunter-X <askzenai@gmail.com>
2024-12-27 05:02:05 +0100

    Add bugSolution.m file

---
+++ bugSolution.m
@@ -0,0 +1,9 @@
+function result = myFunction(input)
+  % Some code here...
+  if someCondition
+    result = someValue;
+  else
+    result = NaN;  % Return NaN instead of []
+  end
+  % More code here...
+end
